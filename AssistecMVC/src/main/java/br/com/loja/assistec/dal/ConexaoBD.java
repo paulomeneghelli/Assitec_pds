9383e64508aa6244eb52d9951bd2ee6bed788e5e
paulostrey <meneghellipaulo9@gmail.com>
2024-11-13 20:50:26 -0300

    ajuste

--- AssistecMVC/src/main/java/br/com/loja/assistec/dal/ConexaoBD.java
+++ AssistecMVC/src/main/java/br/com/loja/assistec/dal/ConexaoBD.java
@@ -3,16 +3,14 @@ package br.com.loja.assistec.dal;
 import java.sql.*;
 
 public class ConexaoBD {
-	// São criadas constantes pois as informações são estáticas
 	private static final String URL_MYSQL = "jdbc:mysql://localhost:3306/lojabd";
 
 	private static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
 
 	private static final String USER = "root";
 
-	private static final String PASSWORD = "Camili1605";
+	private static final String PASSWORD = "aluno";
 
-	// Método para estabelecer conexão com o BD
 	public static Connection conectar() {
 
 		try {
@@ -26,7 +24,6 @@ public class ConexaoBD {
 		return null;
 	}
 	
-	//Método para desconectar do banco e dados
 	public void desconectar(Connection conexao) {
 		try {
 			conexao.close();
